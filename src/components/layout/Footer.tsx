7feccdd2309516ef2a95c1b3d613b2d792782c39
Maribsorensen <148907991+Maribsorensen@users.noreply.github.com>
2025-08-30 16:48:36 +0200

    Feat: continuing react routing and base header+footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,3 +1,14 @@
 export function Footer() {
-  return <footer></footer>;
+  return (
+    <footer>
+      <div>
+        <div>&copy; Holidaze {new Date().getFullYear()}</div>
+        <div>
+          <a href="https://facebook.com">Facebook</a>
+          <a href="https://instagram.com">Instagram</a>
+          <a href="https://twitter.com">Twitter</a>
+        </div>
+      </div>
+    </footer>
+  );
 }
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,3 +1,20 @@
 export function Header() {
-  return <header></header>;
+  return (
+    <header>
+      <nav>
+        <div>Logo</div>
+        <ul>
+          <li>Home</li>
+          <li>Venues</li>
+        </ul>
+        <div>
+          Profile
+          <ul>
+            <li>Login</li>
+            <li>Register</li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
 }
