100389d2844f1397ccfd3706bc8d72b006e76ea3
Maribsorensen <148907991+Maribsorensen@users.noreply.github.com>
2025-08-31 16:42:03 +0200

    Feat: added export functions for pages and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import { Route, Routes } from 'react-router-dom';
 import { Layout } from './components/layout/Layout';
 import { HomePage } from './pages/HomePage';
+import { LoginPage } from './pages/LoginPage';
+import { RegisterPage } from './pages/RegisterPage';
+import { VenueListPage } from './pages/VenueListPage';
+import { VenuePage } from './pages/VenuePage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/venues" element={<VenueListPage />} />
+        <Route path="/venues/:id" element={<VenuePage />} />
         <Route path="*" element={<h1>404 Not Found</h1>} />
       </Route>
     </Routes>
---
+++ src/pages/VenueListPage.tsx
@@ -0,0 +1,3 @@
+export function VenueListPage() {
+  return <div></div>;
+}
