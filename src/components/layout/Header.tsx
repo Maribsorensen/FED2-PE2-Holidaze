5a40f899864664a95e716597fe5c9cad1f865abf
Maribsorensen <148907991+Maribsorensen@users.noreply.github.com>
2025-09-01 22:44:17 +0200

    Feat: basic header styling

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,22 +1,35 @@
+import { FaUser } from 'react-icons/fa6';
 import { NavLink } from 'react-router-dom';
 
 export function Header() {
   return (
-    <header className="bg-primary text-white">
-      <nav>
+    <header className="bg-primary text-white font-headings text-tranform: uppercase">
+      <nav className="container mx-auto flex justify-between items-center p-4">
         <div>Logo</div>
-        <ul>
-          <li>Home</li>
-          <li>Venues</li>
+        <ul className="flex gap-4">
+          <li>
+            <NavLink to="/" className="hover:underline">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/venues" className="hover:underline">
+              Venues
+            </NavLink>
+          </li>
         </ul>
         <div>
-          Profile
+          <FaUser className="text"></FaUser>
           <ul>
             <li>
-              <NavLink to="/login">Login</NavLink>
+              <NavLink to="/login" className="hover:underline">
+                Login
+              </NavLink>
             </li>
             <li>
-              <NavLink to="/register">Register</NavLink>
+              <NavLink to="/register" className="hover:underline">
+                Register
+              </NavLink>
             </li>
           </ul>
         </div>
