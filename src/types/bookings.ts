f530eddb84e04b4d86d59b1102512e3894c39e48
Maribsorensen <148907991+Maribsorensen@users.noreply.github.com>
2025-09-12 14:46:00 +0200

    Feat: user and booking types

---
+++ src/types/bookings.ts
@@ -0,0 +1,8 @@
+export type TBookings = {
+  id: string;
+  dateFrom: string;
+  dateTo: string;
+  guests: number;
+  created: string;
+  updated: string;
+};
---
+++ src/types/user.ts
@@ -0,0 +1,18 @@
+export type TUser = {
+  name: string;
+  email: string;
+  bio: string;
+  banner: {
+    url: string;
+    alt: string;
+  };
+  avatar: {
+    url: string;
+    alt: string;
+  };
+  venueManager: boolean;
+  _count: {
+    venues: number;
+    bookings: number;
+  };
+};
